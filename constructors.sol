17898cf93fba8ae98b4f13a107f89e0db2166f64
annejungers <annejungers@hotmail.com>
2022-12-10 16:40:49 +0100

    constructors 2

--- constructors.sol
+++ constructors.sol
@@ -4,17 +4,19 @@ pragma solidity >= 0.7.0 < 0.9.0;
 /*
 
 CONSTRUCTORS = a special function using  constructor keyword
-It initialises state variables of a contract
+It initialises state variables of a contract. (when we are refering to the state of a contract we are referring to state variables)
 
-Following are key characteristics:
+key characteristics:
 
-1. A contract can have only one contructor. 
+1. A contract can only haveone contructor. 
 
 2. A constructor code is executed once when a contract is created and it is used to initialize contract state 
+    Means that when a contract is deployed, the constructor is used to initialize the contract state
 
 3. After a constructor code is executed, the final code is deployed to the blockchain 
     This final code includes public functions and code reachable through public functions.
-    Constructor code or any internal method used only by constructor are not included in final code
+    Constructor code or any internal method used only by constructor are not included in final code. 
+    So whatever method we write in our constructor, it will not be present in the final code!! it is just used to initialize contract state
 
     A constructor can be either public or internal
 
@@ -24,5 +26,17 @@ Following are key characteristics:
 
 
 */
+
+contract Member{
+
+    string name;
+    uint age;
+
+    //initialize name and age upon deployment
+
+    constructor(string memory _name, uint _age)
+
+
+}
 
 
